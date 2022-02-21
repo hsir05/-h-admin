3ab802f05dd07e8cdfa431facc89edada41f7cb5
haqsir@163.com <haqsir@163.com>
2022-02-21 16:58:05 +0800

    添加mock 数据

--- README.md
+++ README.md
@@ -37,4 +37,34 @@ pnpm dev // 启动
     
     ```
 
-2.
+2. 自动压缩/开启压缩报告
+
+    ```js
+    ollup-plugin-visualizer // 自动打开打包报告
+    vite-plugin-compression // 是否开启压缩
+
+    // 安装
+    pnpm add rollup-plugin-visualizer vite-plugin-compression -D
+
+    import { visualizer } from "rollup-plugin-visualizer"
+    import compressPlugin from 'vite-plugin-compression';
+
+    visualizer({
+        open: true, // 是否直接打开报告
+        gzipSize: true, // 是否显示 gzipSize
+        brotliSize: false // 是否显示brotliSize
+    }),
+    compressPlugin({
+        ext: '.gz', // 选择压缩后的文件名
+        algorithm: 'gzip', // 何中方式压缩 brotliCompress gzip
+        deleteOriginFile: false // 是否删除原文件
+    })
+    ```
+
+3. mock 数据
+
+    ```js
+    pnpm add vite-plugin-mock -D
+
+    
+    ```
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 import Components from "unplugin-vue-components/vite";
 import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
+import { visualizer } from "rollup-plugin-visualizer";
+import compressPlugin from "vite-plugin-compression";
 
 export default defineConfig({
   plugins: [
@@ -14,6 +16,16 @@ export default defineConfig({
         // AntDesign vue 的自动引入，只需要这一句
         // AntDesignVueResolver()
       ],
+    }),
+    visualizer({
+      open: true, // 是否直接打开报告
+      gzipSize: true, // 是否显示 gzipSize
+      brotliSize: false, // 是否显示brotliSize
+    }),
+    compressPlugin({
+      ext: ".gz", // 选择压缩后的文件名
+      algorithm: "gzip", // 何中方式压缩 brotliCompress gzip
+      deleteOriginFile: false, // 是否删除原文件
     }),
   ],
   resolve: {
